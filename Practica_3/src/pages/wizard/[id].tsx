b7d6edb5f8aa92fa047428202dd3d9f0006ecd51
David Arévalo <72552800+davidarev@users.noreply.github.com>
2023-03-20 11:32:01 +0100

    Create [id].tsx

---
+++ Practica_3/src/pages/wizard/[id].tsx
@@ -0,0 +1,9 @@
+import Form_Characteristics_Wizard from "@/src/components/form_characteristics_wizard_";
+
+export default function Caracteristicas_Wizard() {
+    return (
+        <>
+            <Form_Characteristics_Wizard />
+        </>
+    )
+}
